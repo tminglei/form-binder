08e27bf757e1079fb83e53e43a8acb41a118531d
tminglei <tmlneu@gmail.com>
2014-09-02 18:01:14 +0800

    add support for merging json to data map

--- build.sbt
+++ build.sbt
@@ -14,7 +14,7 @@ libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra"        % "2.3.0" % "provided",
   "org.scalatra" %% "scalatra-json"   % "2.3.0" % "provided",
   "org.json4s"   %% "json4s-jackson"  % "3.2.10" % "provided",
-  "org.scalatra" %% "scalatra-scalatest" % "2.3.0" % "test",
+  "org.scalatest" %% "scalatest" % "2.2.2" % "test",
   "org.eclipse.jetty" % "jetty-webapp" % "9.2.1.v20140609" % "container",
   "org.eclipse.jetty" % "jetty-plus" % "9.2.1.v20140609" % "container",
   "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test"
--- src/main/scala/com/github/tminglei/bind/Processors.scala
+++ src/main/scala/com/github/tminglei/bind/Processors.scala
@@ -2,6 +2,8 @@ package com.github.tminglei.bind
 
 import java.util.regex.Pattern
 import scala.util.matching.Regex
+import org.json4s.JsonAST._
+import org.json4s.jackson.JsonMethods
 
 trait Processors {
 
@@ -33,6 +35,34 @@ trait Processors {
 
   def cleanMatched(regex: Regex, replacement: String = ""): PreProcessor = (input: String) => {
     if (input == null) null else regex.replaceAllIn(input, replacement)
+  }
+
+  ////////////////////////////////////  pre-defined bulk pre-processor implementations  /////////////////////////
+
+  def mergeJsonData(sourceKey: String, destPrefix: Option[String] = None): BulkPreProcessor =
+    (data: Map[String, String]) => {
+      val prefix = destPrefix.getOrElse(sourceKey)
+      val json = JsonMethods.parse(data(sourceKey))
+      data ++ mappedJsonData(prefix, json)
+    }
+
+  private def mappedJsonData(prefix: String, json: JValue): Map[String, String] = json match {
+    case JObject(fields) => {
+      fields.map { case (key, value) =>
+        mappedJsonData(Option(prefix).filterNot(_.isEmpty).map(_ + ".").getOrElse("") + key, value)
+      }.foldLeft(Map.empty[String, String])(_ ++ _)
+    }
+    case JArray(values) => {
+      values.zipWithIndex.map { case (value, i) =>
+        mappedJsonData(prefix + "[" + i + "]", value)
+      }.foldLeft(Map.empty[String, String])(_ ++ _)
+    }
+    case JNull => Map.empty
+    case JNothing => Map.empty
+    case JBool(value) => Map(prefix -> value.toString)
+    case JDouble(value) => Map(prefix -> value.toString)
+    case JInt(value) => Map(prefix -> value.toString)
+    case JString(value) => Map(prefix -> value.toString)
   }
 }
 
