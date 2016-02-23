a3e709f25bb7c4f54460c57134336de308040632
Minglei Tu <tmlneu@gmail.com>
2016-02-23 17:23:26 +0800

    move misc interfaces/classes from `Framework` to package file

--- src/main/scala/com/github/tminglei/bind/Framework.scala
+++ src/main/scala/com/github/tminglei/bind/Framework.scala
@@ -3,95 +3,6 @@ package com.github.tminglei.bind
 import FrameworkUtils._
 import org.slf4j.LoggerFactory
 
-import scala.reflect._
-
-/**
- * The Facade class
- */
-case class FormBinder[R](messages: Messages,
-                errProcessor: ErrProcessor[R] = identity[Seq[(String, String)]] _) {
-
-  private val logger = LoggerFactory.getLogger(FormBinder.getClass)
-
-  /**
-   * bind mappings to data, and return an either, holding validation errors (left) or converted value (right)
-   */
-  def bind[T](mapping: Mapping[T], data: Map[String, String], root: String = ""): Either[R, T] = {
-    logger.debug(s"start binding ... from '$root'")
-    mapping.validate(root, data, messages, Options.apply()) match {
-      case Nil  => Right(mapping.convert(root, data))
-      case errs => Left(errProcessor.apply(errs))
-    }
-  }
-
-  /**
-   * bind and validate data, return (processed) errors
-   */
-  def validate[T](mapping: Mapping[T], data: Map[String, String], root: String = ""): R = {
-    logger.debug(s"start validating ... from '$root'")
-    errProcessor.apply(
-      mapping.validate(root, data, messages, Options.apply())
-    )
-  }
-}
-
-///////////////////////////////////////// core interfaces and classes //////////////////////////////////
-/**
- * Some mark traits, used to help ensure the matching of fixtures in a data processing flow/pipe
- */
-sealed trait InputMode
-object SoloInput extends InputMode
-object BulkInput extends InputMode
-object PolyInput extends InputMode
-
-/**
- * A mark trait, to help distinguish ext object from other normals
- */
-trait Extensible extends Cloneable
-
-/**
- * Trait/classes for meta support
- */
-trait Metable[M] {
-  def _meta: M
-}
-
-case class MappingMeta(targetType: ClassTag[_], baseMappings: List[Mapping[_]] = Nil)
-case class ExtensionMeta(name: String, desc: String, params: List[_] = Nil)
-
-final class Ignored[T]
-
-/**
- * Used to transfer config info in the data processing flow
- */
-case class Options(
-  /** whether to check errors as more as possible */
-  eagerCheck: Option[Boolean] = None,
-  /** whether to skip checking untouched empty field/values */
-  skipUntouched: Option[Boolean] = None,
-  /** used to check whether a field was touched by user; if yes, required fields can't be empty */
-  touchedChecker: Option[TouchedChecker] = None,
-  // internal state, only applied to current mapping
-  private[bind] val _label: Option[String] = None,
-  private[bind] val _constraints: List[Constraint] = Nil,
-  private[bind] val _extraConstraints: List[ExtraConstraint[_]] = Nil,
-  private[bind] val _processors: List[PreProcessor] = Nil,
-  private[bind] val _ignoreConstraints: Boolean = false,
-  private[bind] val _inputMode: InputMode = SoloInput,
-  private[bind] val _extData: Option[Extensible] = None
- ) {
-  def eagerCheck(check: Boolean): Options = copy(eagerCheck = Some(check))
-  def skipUntouched(skip: Boolean): Options = copy(skipUntouched = Some(skip))
-  def touchedChecker(touched: TouchedChecker): Options = copy(touchedChecker = Some(touched))
-
-  def $extraConstraints[T] = _extraConstraints.map(_.asInstanceOf[ExtraConstraint[T]])
-
-  def merge(parent: Options): Options = copy(
-    eagerCheck  = eagerCheck.orElse(parent.eagerCheck),
-    skipUntouched = skipUntouched.orElse(parent.skipUntouched),
-    touchedChecker = touchedChecker.orElse(parent.touchedChecker))
-}
-
 /**
  * A mapping, w/ constraints/processors/options, was used to validate/convert input data
  */
@@ -182,7 +93,7 @@ case class GroupMapping[T](fields: Seq[(String, Mapping[_])], doConvert: (String
                 override val options: Options = Options.apply(_inputMode = BulkInput)) extends Mapping[T] {
   private val logger = LoggerFactory.getLogger(GroupMapping.getClass)
 
-  override val _meta = MappingMeta(classTag[Product], Nil)
+  override val _meta = MappingMeta(reflect.classTag[Product], Nil)
   override def options(setting: Options => Options) = copy(options = setting(options))
 
   def convert(name: String, data: Map[String, String]): T = {
--- src/main/scala/com/github/tminglei/bind/package.scala
+++ src/main/scala/com/github/tminglei/bind/package.scala
@@ -50,3 +50,95 @@ package object bind {
     }
   }
 }
+
+package bind {
+
+  import org.slf4j.LoggerFactory
+
+  /**
+    * The Facade class
+    */
+  case class FormBinder[R](messages: Messages,
+    errProcessor: ErrProcessor[R] = identity[Seq[(String, String)]] _) {
+
+    private val logger = LoggerFactory.getLogger(FormBinder.getClass)
+
+    /**
+      * bind mappings to data, and return an either, holding validation errors (left) or converted value (right)
+      */
+    def bind[T](mapping: Mapping[T], data: Map[String, String], root: String = ""): Either[R, T] = {
+      logger.debug(s"start binding ... from '$root'")
+      mapping.validate(root, data, messages, Options.apply()) match {
+        case Nil  => Right(mapping.convert(root, data))
+        case errs => Left(errProcessor.apply(errs))
+      }
+    }
+
+    /**
+      * bind and validate data, return (processed) errors
+      */
+    def validate[T](mapping: Mapping[T], data: Map[String, String], root: String = ""): R = {
+      logger.debug(s"start validating ... from '$root'")
+      errProcessor.apply(
+        mapping.validate(root, data, messages, Options.apply())
+      )
+    }
+  }
+
+  ///////////////////////////////////////// helper interfaces and classes //////////////////////////////////
+  /**
+    * Some mark traits, used to help ensure the matching of fixtures in a data processing flow/pipe
+    */
+  sealed trait InputMode
+  object SoloInput extends InputMode
+  object BulkInput extends InputMode
+  object PolyInput extends InputMode
+
+  /**
+    * A mark trait, to help distinguish ext object from other normals
+    */
+  trait Extensible extends Cloneable
+
+  /**
+    * Trait/classes for meta support
+    */
+  trait Metable[M] {
+    def _meta: M
+  }
+
+  case class MappingMeta(targetType: reflect.ClassTag[_], baseMappings: List[Mapping[_]] = Nil)
+  case class ExtensionMeta(name: String, desc: String, params: List[_] = Nil)
+
+  final class Ignored[T]
+
+  /**
+    * Used to transfer config info in the data processing flow
+    */
+  case class Options(
+    /** whether to check errors as more as possible */
+    eagerCheck: Option[Boolean] = None,
+    /** whether to skip checking untouched empty field/values */
+    skipUntouched: Option[Boolean] = None,
+    /** used to check whether a field was touched by user; if yes, required fields can't be empty */
+    touchedChecker: Option[TouchedChecker] = None,
+    // internal state, only applied to current mapping
+    private[bind] val _label: Option[String] = None,
+    private[bind] val _constraints: List[Constraint] = Nil,
+    private[bind] val _extraConstraints: List[ExtraConstraint[_]] = Nil,
+    private[bind] val _processors: List[PreProcessor] = Nil,
+    private[bind] val _ignoreConstraints: Boolean = false,
+    private[bind] val _inputMode: InputMode = SoloInput,
+    private[bind] val _extData: Option[Extensible] = None
+  ) {
+    def eagerCheck(check: Boolean): Options = copy(eagerCheck = Some(check))
+    def skipUntouched(skip: Boolean): Options = copy(skipUntouched = Some(skip))
+    def touchedChecker(touched: TouchedChecker): Options = copy(touchedChecker = Some(touched))
+
+    def $extraConstraints[T] = _extraConstraints.map(_.asInstanceOf[ExtraConstraint[T]])
+
+    def merge(parent: Options): Options = copy(
+      eagerCheck  = eagerCheck.orElse(parent.eagerCheck),
+      skipUntouched = skipUntouched.orElse(parent.skipUntouched),
+      touchedChecker = touchedChecker.orElse(parent.touchedChecker))
+  }
+}
