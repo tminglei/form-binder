a76b1fa0c9b239f8df33dfe82ff226541ce666fa
Minglei Tu <tmlneu@gmail.com>
2016-04-15 08:09:44 +0800

    add some comments to `Mapping` trait

--- src/main/scala/com/github/tminglei/bind/Framework.scala
+++ src/main/scala/com/github/tminglei/bind/Framework.scala
@@ -7,15 +7,67 @@ import org.slf4j.LoggerFactory
  * A mapping, w/ constraints/processors/options, was used to validate/convert input data
  */
 trait Mapping[T] extends Metable[MappingMeta] {
+
+  /**
+    * get the mapping's options
+    */
   def options: Options = Options.apply()
+
+  /**
+    * set the mapping's options
+    */
   def options(setting: Options => Options) = this
+
+  /**
+    * associate a label the mapping, which will be used when generating error message
+    */
   def label(label: String) = options(_.copy(_label = Option(label)))
+
+  /**
+    * prepend some [[PreProcessor]]s to the mapping, which will be used to pre-process inputting data strings
+    */
   def >-:(newProcessors: PreProcessor*) = options(_.copy(_processors = newProcessors ++: options._processors))
+
+  /**
+    * prepend some [[Constraint]]s to the mapping, which will be used to validate data string before it was converted
+    */
   def >+:(newConstraints: Constraint*) = options(_.copy(_constraints = newConstraints ++: options._constraints))
+
+  /**
+    * append some [[ExtraConstraint]]s to the mapping, which will be used to validate data after it was converted
+    */
   def verifying(validates: ExtraConstraint[T]*) = options(_.copy(_extraConstraints = options._extraConstraints ++ validates))
 
+  ///---
+  /**
+    * do the converting and return the result object if successful
+    * (p.s. to prevent failure, [[validate]] should be called before this)
+    *
+    * @param name the full name of the data node
+    * @param data the data map
+    * @return result object
+    */
   def convert(name: String, data: Map[String, String]): T
+
+  /**
+    * do the validating and return the error message list
+    * (p.s. if successful, the returned list is empty)
+    *
+    * @param name the full name of the data node
+    * @param data the data map
+    * @param messages the messages object
+    * @param parentOptions the parent mapping's options
+    * @return error message list
+    */
   def validate(name: String, data: Map[String, String], messages: Messages, parentOptions: Options): Seq[(String, String)]
+
+  /**
+    * used to map/transform result object to another
+    *
+    * @param transform the transform function
+    * @tparam R target result type
+    * @return a new wrapper mapping
+    */
   def map[R](transform: T => R): Mapping[R] = new TransformMapping[T, R](this, transform)
 }
 
